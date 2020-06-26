d90cb18290634af315188ea3143d99b38725a59d
Peter Muessig <petermuessig@users.noreply.github.com>
2020-06-26 20:39:24 +0200

    Some rollup fixes (#2)

--- packages/ui5-apollo-lib/build/rollup.config.js
+++ packages/ui5-apollo-lib/build/rollup.config.js
@@ -4,45 +4,43 @@ import replace from "@rollup/plugin-replace";
 import resolve from "rollup-plugin-node-resolve";
 import { terser } from "rollup-plugin-terser";
 
-const config = require("../package.json");
+const pkgJson = require("../package.json");
 
-const bundle = {
-	input: "build-src/index.js",
-	output: {
-        file: "dist/resources/apollo/client/thirdparty/apollo.js",
-        format: "amd",
-        amd: {
-            define: "sap.ui.define"
-        }
-    },
-	plugins: [
-		resolve({
-			browser: true,
-			mainFields: ["module", "main"]
-		}),
-		cjs({
-			exclude: "src/**"
-		}),
-		babel({
-			exclude: "node_modules/**"
-		}),
-		replace({
-			VERSION: JSON.stringify(config.version)
-		})
-	]
-};
-
-function minify(config) {
-	let configClone = JSON.parse(JSON.stringify(config));
-	// let configClone = Object.assign({}, config); // deep copy contains references!
-	configClone.output.file = "dist/resources/apollo/client/thirdparty/apollo.min.js";
-	configClone.plugins.push(terser());
-	return configClone;
+function buildConfig(minify) {
+	const config = {
+		input: "build-src/index.js",
+		output: {
+			file: `dist/resources/apollo/client/thirdparty/apollo${minify ? "" : "-dbg"}.js`,
+			format: "amd",
+			amd: {
+				define: "sap.ui.define"
+			}
+		},
+		plugins: [
+			resolve({
+				browser: true,
+				mainFields: ["module", "main"]
+			}),
+			cjs({
+				exclude: "src/**"
+			}),
+			babel({
+				exclude: "node_modules/**"
+			}),
+			replace({
+				VERSION: JSON.stringify(pkgJson.version)
+			})
+		]
+	};
+	if (minify) {
+		config.plugins.push(terser());
+	}
+	return config;
 }
 
 const bundles = [
-	bundle,
-	minify(bundle),
+	buildConfig(),
+	buildConfig(true),
 ];
 
 export default bundles;
